90c43aeeb03d5095cef5a128b81d48cad2bcffa0
Bobby Seidensticker <bseidensticker@gmail.com>
2011-09-03 01:09:22 -0700

    first commit

---
+++ scripts/abstract.js
@@ -0,0 +1,253 @@
+exports.extend({
+    'init': init,
+    'move': move,
+    'world': world,
+    'getPlayerInfo': getPlayerInfo,
+    'getLocalRegion': getLocalRegion,
+    'shoot': shoot,
+    'punch': punch,
+    'buyBullets': buyBullets
+})
+
+var world;  // array of strings
+var storedWorld;
+var player;
+var ui;
+var client;
+var storage;
+var SIZE = 100;
+
+var WORLD_DOC = '_world';
+var WORLD_BLOB = 'world';
+var UPDATES_BLOB = 'updates';
+
+var useMulti = false;
+
+function init(c, userInterface) {
+    client = c;
+    storage = client.storage;
+    ui = userInterface;
+    
+    if (useMulti) {
+        storage.subscribe(WORLD_DOC, WORLD_BLOB, undefined, onRemoteUpdate);
+        storage.getBlob(WORLD_DOC, WORLD_BLOB, undefined, function (result) {
+            onRemoteUpdate(result);
+        });
+    }
+
+    makeWorld();
+    newPlayer();
+}
+
+function getPlayerInfo() {
+    return player;
+}
+
+// returns 15x15 array 
+function getLocalRegion() {
+    var arr = [];
+    var x = player.x - 7;
+    var y = player.y - 7;
+    for (var j = 0; j < 15; j++) {
+        arr[j] = '';
+        for (var i = 0; i < 15; i++) {
+            if (x + i < 0 || y + j < 0 || x + i > SIZE - 1 || y + j > SIZE - 1) {
+                arr[j] += 'w';
+            } else {
+                arr[j] += getWorld(x + i, y + j);
+            }
+        }
+    }
+    return arr;
+}
+
+// strings do not let you edit by index, must slice
+function setWorld(x, y, character) {
+    if (x >= 0 && x < SIZE && y >= 0 && y < SIZE) {
+        world[y] = world[y].slice(0, x) + character + world[y].slice(x + 1);
+    }
+}
+function getWorld(x, y) {
+    if (x >= 0 && x < SIZE && y >= 0 && y < SIZE) {
+        return world[y][x];
+    } else {
+        return false;
+    }
+}
+
+function makeWorld() {
+    // 'b': block (breakable), 's': space
+    var w = [];
+    for (var i = 0; i < SIZE; i++) {
+        w[i] = '';
+        for (var j = 0; j < SIZE; j++) {
+            w[i] += 'b';
+        }
+    }
+    world = w;
+    setWorld(0, 0, 'u');
+}
+
+function updateWorld() {
+    ui.onUpdate();
+    var diffs = diffWorld();
+    if (diffs.length > 0) {
+        storage.push(WORLD_DOC, UPDATES_BLOB, diffs);
+    }
+    if (diffs.length > 100) {
+        var storing = copyWorld(world);
+        storage.putBlob(WORLD_DOC, WORLD_BLOB, {world: world}, undefined, function () {
+            storedWorld = storing;
+        });
+    }
+}
+
+function diffWorld() {
+    var diffs = [];
+    if (!storedWorld) {
+        return diffs;
+    }
+    for (var y = 0; y < SIZE; y++) {
+        for (var x = 0; x < SIZE; x++) {
+            if (storedWorld[y][x] != world[y][x]) {
+                diffs.push({ x: x, y: y, val: world[y][x] });
+            }
+        }
+    }
+    return diffs;
+}
+
+function copyWorld(w) {
+    var copy = [];
+    for (var y = 0; y < SIZE; y++) {
+        copy[y] = w[y];
+    }
+}
+
+function onRemoteUpdate(result) {
+    world = result.world;
+    storedWorld = result.world;
+    updateWorld();
+}
+
+function newPlayer() {
+    var p = {};
+    p.x = 0;
+    p.y = 0;
+    p.bullets = 25;
+    p.health = 1;
+    p.dir = 1;
+    player = p;
+}
+
+// if block in front of player call break block
+function punch() {
+    
+}
+
+function buyBullets() {
+    player.bullets += 1000;
+}
+
+// check if player has bullets, update player.bullets, call breakBlock
+function shoot() {
+    console.log('shoot()  player.dir: ' + player.dir);
+    if(player.bullets > 0)
+    {
+        player.bullets--;
+        bulletx = player.x;
+        bullety = player.y;
+        switch(player.dir){
+        case 1:  // right
+            bulletx++;
+            while (getWorld(bulletx, bullety) == ' ' && bulletx < SIZE) {
+                bulletx++;
+            }
+            if (getWorld(bulletx, bullety) == 'b') {
+                breakBlock(bulletx, bullety);
+            }
+            break;
+        case 3:  // left
+            bulletx--;
+            while (getWorld(bulletx, bullety) == ' ' && bulletx > 0) {
+                bulletx--;
+            }
+            if (getWorld(bulletx, bullety) == 'b') {
+                breakBlock(bulletx,bullety);
+            }
+            break;
+        case 0:  // up
+            bullety--;
+            while (getWorld(bulletx, bullety) == ' ' && bullety > 0) {
+                bullety--;
+            }
+            if (getWorld(bulletx, bullety) == 'b') {
+                breakBlock(bulletx, bullety);
+            }
+            break;
+        case 2:  // down
+            bullety++;
+            while (getWorld(bulletx, bullety) == ' ' && bullety < SIZE) {
+                bullety++;
+            }
+            if (getWorld(bulletx, bullety) == 'b') {
+                breakBlock(bulletx, bullety);
+            }
+            break;
+        }
+    }
+    else
+    {
+        console.log("get user to buy bullets");
+    }    
+    console.log(player.bullets);
+    console.log("bullet end:"+bulletx+","+bullety);
+    console.log(world[bulletx,bullety]);
+
+}
+
+// award 10 bullets to player (5 %) award 100 bullets to player (.5%)
+// alter world[][] to change appropriate 'b' to 's'
+function breakBlock(x, y) {
+    console.log('breakBlock x: ' + x + ' y: ' + y);
+    setWorld(x, y, ' ');
+    updateWorld();
+}
+
+function move(dir) {
+    var needUpdate = false;
+    var x = player.x;
+    var y = player.y;
+    player.dir = dir;
+    switch (dir) {
+    case 0:
+        if (player.y > 0 && getWorld(player.x, player.y - 1) == ' ') {
+            player.y--;
+            needUpdate = true;
+        }
+        break;
+    case 1:
+        if (player.x < SIZE - 1 && getWorld(player.x + 1, player.y) == ' ') {
+            player.x++;
+            needUpdate = true;
+        }
+        break;
+    case 2:
+        if (player.y < SIZE - 1 && getWorld(player.x, player.y + 1) == ' ') {
+            player.y++;
+            needUpdate = true;
+        }
+        break;
+    case 3:
+        if (player.x > 0 && getWorld(player.x - 1, player.y) == ' ') {
+            player.x--;
+            needUpdate = true;
+        }
+        break;
+    }
+    if (needUpdate) {
+        setWorld(x, y, ' ');
+        setWorld(player.x, player.y, 'u');
+    }
+    updateWorld();
+}
---
+++ scripts/main.js
@@ -0,0 +1,188 @@
+var clientLib = require('com.pageforest.client');
+var ms = require('com.pageforest.mazeshooter.abstract');
+
+exports.extend({
+    'main': main,
+    'drawWorld': drawWorld,
+    'onUpdate': onUpdate,
+    'getDocid': getDocid,
+    'buyBullets': buyBullets
+})
+
+function getDocid() {
+    if (client.username) {
+        return client.username;
+    }
+}
+
+var client;
+var app = {
+    // Loading a document
+    setDoc: function(json) {
+        $('#blob').val(json.blob);
+    },
+
+    // Saving a document
+    getDoc: function() {
+        return {
+            "blob": $('#blob').val()
+        };
+    }
+};
+
+function main() {
+    handleAppCache();
+    client = new clientLib.Client(app);
+    client.addAppBar();
+    ms.init(client, exports);
+    drawWorld();
+    $(window).bind('keydown', onKeyDown);
+    guy1.addEventListener('load', function () { drawWorld()});
+}
+
+// For offline - capable applications
+function handleAppCache() {
+    if (typeof applicationCache == 'undefined') {
+        return;
+    }
+
+    if (applicationCache.status == applicationCache.UPDATEREADY) {
+        applicationCache.swapCache();
+        location.reload();
+        return;
+    }
+
+    applicationCache.addEventListener('updateready', handleAppCache, false);
+}
+
+function onUpdate() {
+    console.log("onUpdate");
+    drawWorld();
+}
+
+function onKeyDown(e) {
+    switch (e.keyCode)
+    {
+    case 37: //left
+        e.preventDefault();
+        ms.move(3);
+        break;
+    case 38: //up
+        e.preventDefault();
+        ms.move(0);
+        break;
+    case 39: //right
+        e.preventDefault();
+        ms.move(1);
+        break;
+    case 40: //down
+        e.preventDefault();
+        ms.move(2);
+        break;
+    case 32: //space - shoot
+        e.preventDefault();
+        ms.shoot();
+        break;
+    case 68: //d - shoot
+        e.preventDefault();
+        ms.shoot();
+        break;
+    case 70: //f - punch
+        e.preventDefault();
+        ms.punch();
+    default:
+    }
+}
+
+var guy0 = new Image();
+guy0.src = 'images/guy0.png';
+var guy1 = new Image();
+guy1.src = 'images/guy1.png';
+var guy2 = new Image();
+guy2.src = 'images/guy2.png';
+var guy3 = new Image();
+guy3.src = 'images/guy3.png';
+
+
+function drawWorld() {
+    $('#bulletCount').text('Bullets Remaining: ' + ms.getPlayerInfo().bullets);
+    var tilesize = 24; 
+    var canvas = document.getElementById('canvasWorld');
+    canvas.width = canvas.width;
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = "rgb(255,255,255)";
+    ctx.fillRect (0, 0, 360, 360);
+
+    var dir = ms.getPlayerInfo().dir;
+    var map = ms.getLocalRegion();
+    for (var i = 0; i < 15; i++) {
+        for(var j = 0; j < 15; j++) {
+            var line = map[i];
+
+            if (line.charAt(j) == "u") {
+                ctx.fillStyle = "rgb(255, 255, 255)";
+            } else if(line.charAt(j) == "w") {
+                ctx.fillStyle = "rgb(50, 0, 0)";
+            } else if (line.charAt(j) == "b") {
+                ctx.fillStyle = "rgb(0, 0, 200)";
+            } else {
+                ctx.fillStyle = "rgb(255, 255, 255)";
+            }
+            ctx.fillRect (j * tilesize, i*tilesize,
+                     (j+1)*tilesize, (i+1)*tilesize);
+            
+            /*if (line.charAt(j) == "b") {
+                ctx.drawImage(block, i*tilesize, j*tilesize);
+            }*/
+        
+        if(line.charAt(j) == "u" || 
+           line.charAt(j) == "r" || 
+           line.charAt(j) == "m"){
+        //draw triangle indicating direction
+                //ctx.fillStyle = "rgb(0,0,0)";
+                switch(dir) {
+                    case 0:
+                    ctx.drawImage(guy0, i*tilesize, j*tilesize);
+                    break;
+                    case 1:
+                    ctx.drawImage(guy1, i*tilesize, j*tilesize);
+                    break;
+                    case 2:
+                    ctx.drawImage(guy2, i*tilesize, j*tilesize);
+                    break;
+                    case 3:
+                    ctx.drawImage(guy3, i*tilesize, j*tilesize);
+                    break;
+                    
+                }
+                ctx.fill();    
+            }
+        }
+    }
+    console.log(map);
+}
+
+
+function buyBullets() {
+  goog.payments.inapp.buy({
+    'jwt'     :     "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIxMTY4MTE1NjE0MDU5N" + 
+               "Tk1NDQ0MCIsImF1ZCI6Ikdvb2dsZSIsInR5cCI6Imdvb2dsZS9" + 
+               "wYXltZW50cy9pbmFwcC9pdGVtL3YxIiwiaWF0IjoxMzEyODQ2N" + 
+               "DAwLCJleHAiOjEzMTI5MzI4MDAsInJlcXVlc3QiOnsiY3VycmV" + 
+               "uY3lDb2RlIjoiVVNEIiwicHJpY2UiOiIwLjk5IiwibmFtZSI6I" + 
+               "kJ1Y2tldC1vLUJ1bGxldHMiLCJzZWxsZXJEYXRhIjoiMTAwIiw" + 
+               "iZGVzY3JpcHRpb24iOiJMb2FkIHVwIHlvdXIgbWFnYXppbmUgZ" + 
+               "m9yIE1pbmVTaG9vdGVyIn19.lyoZZ1U6Y6BYVJpAJ8M2XEKqIk" + 
+               "G1s1lNz7HQjoi-Jp4",
+    'success' : onBulletPurchase,
+    'failure' : function () {
+        alert("Your purchase failed");
+    }
+  });
+}
+
+//Success handler
+function onBulletPurchase(purchaseAction) {
+    alert("Purchase success!");
+    ms.buyBullets();
+}
